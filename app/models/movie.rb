ae7b4700a5baad5f54ebe3c38804b7509fa60387
SuperTikuwa <tikuwamk2@gmail.com>
2021-09-22 18:34:19 +0900

    station2

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,3 @@
 class Movie < ApplicationRecord
+  validates :name, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 50 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/movies" => "movies#index"
   get "/admin/movies" => "admin/movies#index"
+  get "/admin/movies/new" => "admin/movies#new"
+  post "/admin/movies/new" => "admin/movies#create"
 end
