200b11c7612156c2c4c6971697fd2807a50b8e1f
SuperTikuwa <tikuwamk2@gmail.com>
2021-09-22 18:41:22 +0900

    station2

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,7 @@
 class Movie < ApplicationRecord
-  validates :name, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 50 }
+  validates :name, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 160 }
+  validates :year, presence: true, numericality: { only_integer: true, greater_than: 0 }, length: { maximum: 45 }
+  validates :description, presence: true, length: { maximum: 500 }
+  validates :image_url, presence: true, length: { maximum: 150 }
+  validates :is_showing, presence: true
 end
