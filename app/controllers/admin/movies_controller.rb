80a2590dce097c1768c65c5639ea12f827924b1b
SuperTikuwa <tikuwamk2@gmail.com>
2021-09-23 22:10:25 +0900

    station4

--- app/controllers/admin/movies_controller.rb
+++ app/controllers/admin/movies_controller.rb
@@ -29,6 +29,6 @@ class Admin::MoviesController < ApplicationController
   private
 
   def movie_params
-    params.require(:movie).permit(:name, :year, :description, :image_url, :is_showing)
+    params.permit(:name, :year, :description, :image_url, :is_showing)
   end
 end
